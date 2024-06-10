80f62339b8bee34cb2ddc2e2a631d230cff36aae
richardyilin <richard19990101@gmail.com>
2024-06-09 20:58:47 -0700

    all writethrough

--- hw/rtl/VX_cluster.sv
+++ hw/rtl/VX_cluster.sv
@@ -103,7 +103,7 @@ module VX_cluster import VX_gpu_pkg::*; #(
         .MEM_OUT_BUF    (2),
         .NC_ENABLE      (1),
         .PASSTHRU       (!`L2_ENABLED),
-        .WRITEBACK      (1)
+        .WRITEBACK      (0)
     ) l2cache (
         .clk            (clk),
         .reset          (l2_reset),
